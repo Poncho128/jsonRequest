df13e0b783a18bfc0c7cdaee5fd423a31f7ff3bf
Poncho <alfonsoruvera@gmail.com>
2018-03-22 14:45:53 -0600

    called_JSON_from_MA

--- app/src/main/java/mx/itesm/jsonrequest/MainActivity.java
+++ app/src/main/java/mx/itesm/jsonrequest/MainActivity.java
@@ -2,12 +2,11 @@ package mx.itesm.jsonrequest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
 
-    public void loadMyAmazingJSON(){
-
-    }
+public class MainActivity extends AppCompatActivity implements AsyncJsonHandler.JSONListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void loadMyAmazingJSON(View view){
+        AsyncJsonHandler asyncJsonHandler = new AsyncJsonHandler(this);
+        asyncJsonHandler.execute("https://raw.githubusercontent.com/Poncho128/Poncho128.github.io/master/friends.json");
+    }
 
+
+    @Override
+    public void requestDone(JSONArray jsonArray) {
+
+    }
 }
